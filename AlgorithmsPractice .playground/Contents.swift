915fd5a79f35ace0f9df442c037122a41479fa50
zhany <zhanybeksalgarin@gmail.com>
2023-09-03 11:03:42 -0700

    Max area linear solution

--- AlgorithmsPractice .playground/Contents.swift
+++ AlgorithmsPractice .playground/Contents.swift
@@ -2193,3 +2193,29 @@ import UIKit
 //        return result
 //    }
 //}
+
+
+// liniar solution.
+
+//class Solution {
+//    func maxArea(_ height: [Int]) -> Int {
+//        guard !height.isEmpty else { return 0 }
+//
+//        var result = 0
+//        var left = 0
+//        var right = height.count - 1
+//
+//        while left < right {
+//            let minimum = min(height[left], height[right])
+//            let area = (right - left) * minimum
+//            result = max(result, area)
+//
+//            if height[left] < height[right] {
+//                left += 1
+//            } else {
+//                right -= 1
+//            }
+//        }
+//        return result
+//    }
+//}
