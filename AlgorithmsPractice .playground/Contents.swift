a1458211311e8ebe2d45d2babc98964a15f2d200
zhany <zhanybeksalgarin@gmail.com>
2024-06-03 06:38:10 -0700

    Same Tree

--- AlgorithmsPractice .playground/Contents.swift
+++ AlgorithmsPractice .playground/Contents.swift
@@ -3548,3 +3548,26 @@ class Solution {
 //        return max(left, right)
 //    }
 //}
+
+
+
+// MARK: -Same Tree
+
+
+//
+//class Solution {
+//    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+//        if p == nil && q == nil {
+//            return true
+//        }
+//        if p?.val != q?.val {
+//            return false
+//        }
+//        if p == nil || q == nil {
+//            return false
+//        }
+//        return (isSameTree(p?.left, q?.left) &&
+//        isSameTree(p?.right, q?.right))
+//        
+//    }
+//}
