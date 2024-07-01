0088242f4745a32800327f0196f426511e6e010c
zhany <zhanybeksalgarin@gmail.com>
2024-07-01 12:53:14 -0700

    Binary Tree Level Order Traversal (Practice BFS traversal)

--- AlgorithmsPractice .playground/Contents.swift
+++ AlgorithmsPractice .playground/Contents.swift
@@ -4075,30 +4075,30 @@ class Solution {
 //}
 
 
-//MARK: -Binary Tree Level Order Traversal (Practice)
+//MARK: -Binary Tree Level Order Traversal (Practice BFS)
 
-class Solution {
-    func levelOrder(_ root: TreeNode?) -> [[Int]] {
-        guard let root = root else { return [] }
-
-        var queue = [root]
-        var result = [[Int]]()
-
-        while !queue.isEmpty {
-            var level = [Int]()
-            for _ in 0..<queue.count {
-                let node = queue.removeFirst()
-                level.append(node.val)
-
-                if let left = node.left {
-                    queue.append(left)
-                }
-                if let right = node.right {
-                    queue.append(right)
-                }
-            }
-            result.append(level)
-        }
-        return result
-    }
-}
+//class Solution {
+//    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+//        guard let root = root else { return [] }
+//
+//        var queue = [root]
+//        var result = [[Int]]()
+//
+//        while !queue.isEmpty {
+//            var level = [Int]()
+//            for _ in 0..<queue.count {
+//                let node = queue.removeFirst()
+//                level.append(node.val)
+//
+//                if let left = node.left {
+//                    queue.append(left)
+//                }
+//                if let right = node.right {
+//                    queue.append(right)
+//                }
+//            }
+//            result.append(level)
+//        }
+//        return result
+//    }
+//}
